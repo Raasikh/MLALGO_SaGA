5b4498bf735cb2029ddfed78f822655a3664d916
Raasikh Ahmed Naveed <101375636+Raasikh@users.noreply.github.com>
2025-06-01 13:39:48 -0500

    Add files via upload

---
+++ trainLG.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+class LogisticRegression:
+    def __init__(self, n_iter=1000, lr=0.001):
+        self.n_iter=n_iter
+        self.lr=lr
+        self.weights=None
+        self.bias=None
+    
+    def fit(self, X,y):
+        n_samples, n_features = X.shape
+        self.weights= np.zeros(n_features)
+        self.bias=0
+
+        for _ in range(self.n_iter):
+            lr_predictions=np.dot(X, self.weights)+self.bias
+            predictions=sigmoid(lr_predictions)
+            
+            dw=1/(n_samples) * np.dot(X.T, (predictions-y))
+            db=1/(n_samples) *np.sum(predictions-y)
+
+            self.weights= self.weights- self.lr*dw
+            self.bias=self.bias - self.lr*db
+
+
+
+
+    def predict(self, X):
+        lr_predictions=np.dot(X, self.weights)+self.bias
+        y_pred=sigmoid(lr_predictions)
+        class_pred=[0 if y<=0.5 else 1 for y in y_pred]
+        return class_pred
